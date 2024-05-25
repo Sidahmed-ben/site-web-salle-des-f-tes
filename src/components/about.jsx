7f1227ddb42d461e21796cac95da66f37b7a61ed
sidahmed.benaissa <benaissas886@gmail.com>
2024-05-25 13:24:37 +0200

    suppression de commentaire

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,17 +3,32 @@ import React from "react";
 export const About = (props) => {
   return (
     <div id="about">
-      <div className="container">
+      <div className="container " class="container-sm">
         <div className="row">
-          <div className="col-xs-12 col-md-6">
+          <div className="col-xs-12 col-md-5">
             {" "}
-            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+            <img src="img/about.jpeg" className="img-responsive" alt="" />{" "}
           </div>
-          <div className="col-xs-12 col-md-6">
+          <div className="col-xs-12 col-md-7">
             <div className="about-text">
-              <h2>About Us</h2>
-              <p>{props.data ? props.data.paragraph : "loading..."}</p>
-              <h3>Why Choose Us?</h3>
+              <h2>On vous propose</h2>
+              <p>
+                Bienvenue à Babylone House, notre salle des fêtes et espace de
+                détente, le cadre idéal pour toutes vos occasions spéciales. Que
+                ce soit pour célébrer un mariage, organiser une réunion de
+                famille, ou simplement profiter d'un week-end de relaxation,
+                notre établissement vous offre tout ce dont vous avez besoin.
+                <br />
+                <br />
+                Un vrai havre de paix à 1h de route de Paris, venez comme vous
+                êtes, draps et serviettes seront fournis, toutes les commodités
+                se trouvent à moins de 3km.
+                <br></br>
+                <br></br>
+                Plongez dans notre piscine, détendez-vous dans notre hammam ou
+                profitez de notre jacuzzi pour un moment de bien-être absolu.
+              </p>
+              <h3>Pourquoi Choisir Babylone House ?</h3>
               <div className="list-style">
                 <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,29 +1,43 @@
 import React from "react";
-
 export const Services = (props) => {
   return (
     <div id="services" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Our Services</h2>
-          <p>
+          <h2>Nos Prestations</h2>
+          {/* <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
             dapibus leonec.
-          </p>
+          </p> */}
         </div>
         <div className="row">
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4">
-                  {" "}
-                  <i className={d.icon}></i>
-                  <div className="service-desc">
-                    <h3>{d.name}</h3>
-                    <p>{d.text}</p>
-                  </div>
-                </div>
-              ))
-            : "loading"}
+          <div class="col-md-4">
+            <div class="profile-card-2 fete">
+              {/* <img src="img/services/fete.jpg" className="img img-responsive" /> */}
+              <div class="profile-name">Fêtes</div>
+              <div class="profile-username">@johndoesurname</div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="profile-card-2 bien-etre">
+              {/* <img
+                src="img/services/bien-etre.jpg"
+                className="img img-responsive"
+              /> */}
+              <div class="profile-name">Bien être</div>
+              <div class="profile-username">@johndoesurname</div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="profile-card-2 hebergement">
+              {/* <img
+                src="https://images.caradisiac.com/images/2/1/0/6/172106/S0-mercedes-amg-classe-a-35-un-prix-de-50-400-eur-569831.jpg"
+                className="img img-responsive"
+              /> */}
+              <div class="profile-name">Hébergement</div>
+              <div class="profile-username">@johndoesurname</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
